fb51f839533f625a8bafa86541ec98d94d66890e
Huaqi Fang <578567190@qq.com>
2021-12-03 11:21:17 +0800

    ci: update for linux toolchain build
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/build_linux_toolchain.sh
+++ scripts/build_linux_toolchain.sh
@@ -5,8 +5,20 @@ set -o pipefail
 
 MODE=${MODE:-newlib}
 TARGET=${TARGET:-rv32imafdcbpv-ilp32d}
-TARGET_PREFIX=${TARGET_PREFIX:-/opt/riscv}
 CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA:-$(git rev-parse --short HEAD)}
+REPODIR=${REPODIR:-$(pwd)}
+INDOCKER=0
+
+if [ -f "/.dockerenv" -o -f "/run/.containerenv" ] ; then
+    echo "This is a docker environment"
+    INDOCKER=1
+    TARGET_PREFIX=${TARGET_PREFIX:-/opt/riscv}
+    BUILD_ROOT=${BUILD_ROOT:-/builds}
+else
+    TARGET_PREFIX=${TARGET_PREFIX:-$REPODIR/riscv}
+    BUILD_ROOT=${BUILD_ROOT:-$REPODIR/builds}
+fi
+TOOLCHAIN_TARGZ=${TOOLCHAIN_TARGZ:-$REPODIR/nuclei_${MODE}_${TARGET}_toolchain_${CI_COMMIT_SHORT_SHA}.tar.gz}
 
 echo "Build Nuclei GNU Toolchain for ${MODE} - ${TARGET}"
 
@@ -30,14 +42,28 @@ function strip_toolchain_lin() {
     set -e
 }
 
+function describe_repo {
+    local repodir=${1}
+    local repodesc=${2:-gitrepo.txt}
+
+    if [ -d ${repodir}/.git ] ; then
+        pushd ${repodir}
+        git describe  --always --abbrev=10 --dirty > ${repodesc}
+        git log --oneline -1 >> ${repodesc}
+        git submodule >> ${repodesc}
+        popd
+    else
+        echo "not a git repo" > ${repodesc}
+    fi
+}
+
 TARGET_TUPLE=($(echo ${TARGET} | tr "-" "\n"))
 TARGET_CONF="--with-arch=${TARGET_TUPLE[0]} --with-abi=${TARGET_TUPLE[1]}"
 
 echo "Build Target Configuration : $TARGET_CONF"
-BUILD_OUTDIR=/build/$(date -u +"%Y-%m-%dT%H%M%S")
-# BUILD_OUTDIR=$(pwd)
-BUILD_LOG=$(pwd)/build.log
-TOOLCHAIN_CONFIGURE=$(pwd)/configure
+BUILD_OUTDIR=$BUILD_ROOT/$(date -u +"%Y-%m-%dT%H%M%S")
+BUILD_LOG=$REPODIR/build.log
+TOOLCHAIN_CONFIGURE=$REPODIR/configure
 
 mkdir -p $BUILD_OUTDIR
 pushd $BUILD_OUTDIR
@@ -47,13 +73,17 @@ if [ -d $TARGET_PREFIX ] ; then
     echo "STEP 1: Remove existing prebuilt toolchain"
     rm -rf $TARGET_PREFIX
     mkdir -p $TARGET_PREFIX
+else
+    mkdir -p $TARGET_PREFIX
 fi
+REPODESC=$TARGET_PREFIX/gitrepo.txt
 
+describe_repo "$REPODIR" "$REPODESC"
 $TOOLCHAIN_CONFIGURE --prefix=$TARGET_PREFIX $TARGET_CONF
 
 echo "STEP 2: Build toolchain"
 sed -i -e 's/make_tuple = riscv$(1)-unknown-$(2)/make_tuple = riscv-nuclei-$(2)/g' Makefile
-make -j 4 ${MODE} > $BUILD_LOG 2>&1
+make -j 4 ${MODE} 2>&1 | tee $BUILD_LOG | grep -E "/configure|stamps"
 tail -n20 $BUILD_LOG
 popd
 
@@ -61,5 +91,4 @@ echo "STEP 3: Strip toolchain"
 strip_toolchain_lin $TARGET_PREFIX
 
 echo "STEP 4: Archive toolchain"
-TOOLCHAIN_TARGZ=nuclei_${MODE}_${TARGET}_toolchain_${CI_COMMIT_SHORT_SHA}.tar.gz
 tar --transform "s/^opt\///" -czf $TOOLCHAIN_TARGZ $TARGET_PREFIX
